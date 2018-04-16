94207664078adb4cf1eaa494a6d5c0f218633a4d
Rich Greenwood <w.del@qq.com>
2018-04-16 17:50:09 +0800

    I'm too lazy, nothing left

--- config.fish
+++ config.fish
@@ -14,12 +14,35 @@ function ll
     ls -a -l $argv
 end
 
-function pull
-    git fetch
-    git rebase origin master
+function pull --argument remote branch
+    if test -n "$branch"
+        git fetch $remote $branch
+        git merge $remote/$branch
+    else if test -n "$remote"
+        git fetch origin $remote
+        git merge origin/$remote
+    else
+        git fetch origin master
+        git merge origin/master
+    end
 end
 
-function push
+function push --argument remote branch
+    if test -d .git
+        # git add .
+        git commit -am "I'm too lazy, nothing left"
+        if test -n "$branch"
+            git push $remote $branch
+        else if test -n "$remote"
+            git push origin $remote
+        else
+            git push origin master
+        end
+    else
+        set_color --bold red
+        echo "这不是一个git代码库:" $PWD 
+        set_color normal
+    end
 end
 
 function start
