f0ee9f27946dcd080f138ac251a29453a7cba1c7
Deron.W <w.del@qq.com>
2018-05-10 17:23:27 +0800

    Update setup-vim.sh

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,13 +1,13 @@
 set -e
-echo "\033[47;30m cleanup .vimrc and .vim \033[0m"
+echo "Cleanup .vimrck and .vin/"
 rm -rf ~/.vimrc ~/.vim
 
 # download Vundle first
-echo "\033[47;30m 下载Vundle插件 \033[0m"
+echo "download Vundle plugin first"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # create vim config file
-echo "\033[47;30m create .vimrc file \033[0m"
+echo "create new .vimrc file"
 touch ~/.vimrc
 
 echo "\033[47;30m download config content \033[0m"
@@ -20,7 +20,7 @@ curl -s https://raw.githubusercontent.com/DeronW/terminal/master/vim/plugin.end.
 curl -s https://raw.githubusercontent.com/DeronW/terminal/master/vim/functions.vimrc >> ~/.vimrc
 curl -s https://raw.githubusercontent.com/DeronW/terminal/master/vim/foot.vimrc >> ~/.vimrc
 
-echo "\033[47;30m start install plugin \033[0m"
+echo "starting install Vim plugins"
 vim +PluginInstall +qall # install vim plugin
 
-echo "\033[32m Vim Plugin installed \033[0m"
+echo "\033[32m Nice try! \033[0m"
