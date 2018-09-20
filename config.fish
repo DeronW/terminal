c8157f18be8b1bf84260803bd2a9cefd1c0f41d8
Deron.W <w.del@qq.com>
2018-09-20 15:38:55 +0800

    Update fish shel config. Auto pull/push current git branch

--- config.fish
+++ config.fish
@@ -22,25 +22,28 @@ function pull --argument remote branch
         git fetch origin $remote
         git merge origin/$remote
     else
-        git fetch origin master
-        git merge origin/master
+        set BR (git describe --contains --all HEAD)
+        git fetch origin $BR
+        git merge origin/$BR
     end
 end
 
 function push --argument remote branch
     if test -d .git
         # git add .
-        git commit -am "I'm too lazy, nothing left"
+        set COMMENT (git diff --shortstat)
+        git commit -a -m "No Comment" -m $COMMENT
         if test -n "$branch"
             git push $remote $branch
         else if test -n "$remote"
             git push origin $remote
         else
-            git push origin master
+            set BR (git describe --contains --all HEAD)
+            git push origin $BR
         end
     else
         set_color --bold red
-        echo "这不是一个git代码库:" $PWD 
+        echo "这不是一个git代码库:" $PWD
         set_color normal
     end
 end
